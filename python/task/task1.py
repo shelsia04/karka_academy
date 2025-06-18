1fd3af91a168313b1e4e82c49a83f69779a5dd4e
SHELSIA RG <shelsiarg04@gmail.com>
2025-06-18 19:09:54 +0530

    Add files via upload

---
+++ python/task/task1.py
@@ -0,0 +1,23 @@
+#p1
+print("Hello World")
+#p2
+name="SHELSIA R G"
+print(name)
+#p3
+name="SHELSIA R G"
+age=20
+print(name)
+print(age)
+#p4
+int=4440
+str="hi bruh"
+print(int)
+print(str)
+#p5
+name = "SHELSIA"
+age = 20
+print("Her name is " , name , " and she is " , age , " now.")
+
+
+
+
